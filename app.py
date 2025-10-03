044c2c309c41e98e19724aa106807b3933aa64f5
Tega Aminode <tegagreataminode@gmail.com>
2025-10-03 01:23:12 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask,render_template,request,flash,send_file
+import extractor
+from werkzeug.utils import secure_filename
+import os
+
+app = Flask(__name__)
+app.secret_key = 'tega_secret_key'
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return "Error 404",404
+
+@app.route("/", methods=['GET','POST'])
+def file_upload():
+    if request.method == 'POST':
+       file = request.files['file']
+       file.save(f"{secure_filename(file.filename)}")
+       output_message = extractor.main(file.filename)
+       if output_message:
+            flash(output_message)
+            #download(f)
+    return render_template('home.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ extractor.py
@@ -0,0 +1,78 @@
+from google import genai
+import sys
+
+# Set Gemini API Key
+client = genai.Client(api_key='AIzaSyDY9Z3Epdcvopjn_YV5v9rxJTDdPkI7lR0')
+
+# System instruction
+class SystemMessage():
+    def __init__(self):
+        self.instruction = """Extract the table and its data accurately as csv format"""
+        self.table_validation = """Check if the table has merged columns, merged rows or both.\n
+        Table Validate:\n
+        Iterate through these and respond with the answer that exactly suits the table condition.\n
+        1. If table has merged columns: Yes, merged column,\n
+        2. If table has merged rows: Yes, merged rows\n
+        3. If table has both merged columns and merged rows: Yes, merged column and merged rows\n
+        4. Else: No, merged rows or merged columns"""
+
+def main(i):
+    # Construct class systemMessage
+    sysMessage = SystemMessage()
+    r,mr = ocr_extractor(i,sysMessage.instruction,sysMessage.table_validation)
+    file_names = ['output_merged.csv','output.csv','output_merged_nothing.csv']
+
+    if mr.text  == "Yes, merged column and merged rows":
+        print('Saving merged table now...')
+        try:
+            with open(file_names[0],'w') as file:
+                file.write(r.text)
+                return f'File Saved as {file_names[0]}'
+        except PermissionError:
+            sys.exit("Close file")
+
+    elif mr.text == "No, merged rows or merged columns":
+        print('Saving now...')
+        # Save generated output
+        try:
+            with open(file_names[1],'w') as file:
+                file.write(r.text)
+                return f'File Saved as {file_names[1]}'
+        except PermissionError:
+            sys.exit("Close file")
+    else:
+        print('Saving now...')
+        try:
+            with open(file_names[2],'w') as file:
+                file.write(r.text)
+                return f'File Saved as {file_names[2]}'
+        except PermissionError:
+            sys.exit("Close file")
+
+# Upload your file
+def user_input():
+    try:
+        if len(sys.argv) == 2:
+            uploaded_file = client.files.upload(file=sys.argv[1])
+            return uploaded_file
+        else:
+            return sys.exit("Wrong file")
+    except ValueError:
+      return exit("Wrong Input")
+# Extract table with model
+def ocr_extractor(f,instruction,table_validate):
+    # Extract simple header table's using AI model
+    response = client.models.generate_content(
+        model = 'gemini-2.5-flash',
+        contents = [instruction, f]
+    )
+
+    # Extract merged rows and columns table
+    merged_response = client.models.generate_content(
+        model='gemini-2.5-flash',
+        contents=[table_validate, f]
+    )
+    return response,merged_response
+
+if __name__ == '__main__':
+    main()
